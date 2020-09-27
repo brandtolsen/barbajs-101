ed219bf7ba8b6400bd4aa600e2e4ca23ff2a1efe
Daniel Brandt-Olsen <danbr@itu.dk>
2020-09-27 13:01:32 +0200

    once hook

--- barba-css/src/js/app.js
+++ barba-css/src/js/app.js
@@ -1,1 +1,23 @@
+import barba from '@barba/core';
+import barbaCss from '@barba/css';
 
+// tell Barba to use the css plugin
+barba.use(barbaCss);
+
+// init Barba
+barba.init({
+    transitions: [
+        {
+            name: 'home',
+            beforeOnce(){
+                console.log('beforeOnce')
+            },
+            once(){
+                console.log('once')
+            },
+            afterOnce(){
+                console.log('afterOnce')
+            },
+        }
+    ]
+});
